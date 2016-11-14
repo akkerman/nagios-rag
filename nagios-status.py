07a679ac2118e83a7e84420c047aad4ec67647c8
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2016-11-14 20:49:57 +0100

    translate nagios stat to rag

--- nagios-status.py
+++ nagios-status.py
@@ -1,3 +1,17 @@
-from subprocess import call
+#!/usr/bin/python
+from subprocess import check_output
 
-call(["nagios3stats", "-m", "--data=NUMHOSTS,NUMHSTUP,NUMHSTDOWN,NUMSERVICES,NUMSVCOK,NUMSVCCRIT", "-D,"])
+output = check_output(["nagios3stats", "-m", "--data=NUMHOSTS,NUMHSTUP,NUMHSTDOWN,NUMSERVICES,NUMSVCOK,NUMSVCCRIT", "-D,"])
+
+
+hosts_tot,hosts_up,hosts_down,srv_tot,srv_ok,srv_crit,z = output.split(",")
+
+def color(tot,ok,nok):
+	if tot == ok:
+		return 'green'
+	if nok == '0':
+		return 'orange'
+	return 'red'
+
+print 'Hosts    :', color(hosts_tot,hosts_up,hosts_down)
+print 'Services :', color(srv_tot,srv_ok,srv_crit)
