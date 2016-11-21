e037541b55a8e929b04e1484d59ff92ef96979ab
Marcel Akkerman <marcel.akkerman@gmail.com>
2016-11-21 21:03:06 +0000

    test each rag in its own process

--- test.py
+++ test.py
@@ -5,46 +5,47 @@ import time
 from multiprocessing import Process
 
 def test_cpu():
-    light(cpu_leds, "001")
-    time.sleep(1)
-    light(cpu_leds, "011")
-    time.sleep(1)
-    light(cpu_leds, "010")
-    time.sleep(1)
-    light(cpu_leds, "110")
-    time.sleep(1)
-    light(cpu_leds, "100")
-    time.sleep(1)
-    light(cpu_leds, "000")
+    while True:
+        light(cpu_leds, "001")
+        time.sleep(1)
+        light(cpu_leds, "011")
+        time.sleep(1)
+        light(cpu_leds, "010")
+        time.sleep(1)
+        light(cpu_leds, "110")
+        time.sleep(1)
+        light(cpu_leds, "100")
+        time.sleep(1)
+        light(cpu_leds, "000")
 
-def test_hs(leds):
-    light(leds, "001")
-    time.sleep(2)
-    light(leds, "010")
-    time.sleep(2)
-    light(leds, "100")
-    time.sleep(1)
-    light(leds, "000")
+def test_hs(leds, delay):
+    while True:
+        light(leds, "001")
+        time.sleep(delay)
+        light(leds, "010")
+        time.sleep(delay)
+        light(leds, "100")
+        time.sleep(1)
+        light(leds, "000")
 
 def test_host():
-    test_hs(host_leds)
+    test_hs(host_leds, 2)
 
 def test_serv():
-    test_hs(serv_leds)
+    test_hs(serv_leds, 1.9)
  
 if __name__ == '__main__':
     setup()
     try: 
-        while True:
-            c = Process(target=test_cpu)
-            h = Process(target=test_host)
-            s = Process(target=test_serv)
+        c = Process(target=test_cpu)
+        h = Process(target=test_host)
+        s = Process(target=test_serv)
 
-            c.start()
-            h.start()
-            s.start()
-            c.join()
-            h.join()
-            s.join()
+        c.start()
+        h.start()
+        s.start()
+        c.join()
+        h.join()
+        s.join()
     except KeyboardInterrupt:
         GPIO.cleanup()
